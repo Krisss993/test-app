c29d51cb519a9bd5df1670602c8c13cd21393ff1
kpas9 <kristopherp994@gmail.com>
2024-09-18 12:01:53 +0200

    first commit

--- Django_React_Langchain_Stream/settings.py
+++ Django_React_Langchain_Stream/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'ml-chat.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'ml-chat.onrender.com','test-app-y0lp.onrender.com']
 
 CORS_ALLOW_ALL_ORIGINS = True
 
